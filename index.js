2ae33d3f2644bfe0cea81a6144f031e2498911cc
abc <chihiro.hoya@gmail.com>
2023-06-06 15:27:47 +0900

    日付を削除

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const http = require('node:http');
 const pug = require('pug');
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
